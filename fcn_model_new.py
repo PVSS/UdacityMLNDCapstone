e005dc4504dfafa84512f98c5a55aed24a5f54ef
PVSS <prakashpvss@gmail.com>
2017-08-06 10:46:42 +0530

    Fully Convolutional Network Model U-Net

---
+++ fcn_model_new.py
@@ -0,0 +1,85 @@
+from keras import optimizers
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose,concatenate
+from keras.layers import Dropout, Lambda
+from keras import backend as K
+
+
+def mvn(tensor):
+    '''Performs per-channel spatial mean-variance normalization.'''
+    epsilon = 1e-6
+    mean = K.mean(tensor, axis=(1,2), keepdims=True)
+    std = K.std(tensor, axis=(1,2), keepdims=True)
+    mvn = (tensor - mean) / (std + epsilon)
+    return mvn
+    
+def dice_coef(y_true, y_pred, smooth=0.0):
+    '''Average dice coefficient per batch.'''
+    axes = (1,2,3)
+    intersection = K.sum(y_true * y_pred, axis=axes)
+    summation = K.sum(y_true, axis=axes) + K.sum(y_pred, axis=axes)
+    
+    return K.mean((2.0 * intersection + smooth) / (summation + smooth), axis=0)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred, smooth=10.0)
+
+
+def get_unet(input_shape, num_classes, weights=None):
+    inputs = Input(shape=input_shape, dtype='float', name='data')
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = Lambda(mvn)(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = Lambda(mvn)(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = Lambda(mvn)(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = Lambda(mvn)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    drop1 = Dropout(rate=0.5, name='drop1')(pool4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(drop1)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = Lambda(mvn)(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = Lambda(mvn)(conv6)
+    
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = Lambda(mvn)(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+    conv8 = Lambda(mvn)(conv8)
+    conv8 = Dropout(rate=0.5, name='drop2')(conv8)
+    
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+    conv9 = Lambda(mvn)(conv9)
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+    sgd = optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss=dice_coef_loss, metrics=[dice_coef])
+
+    return model
+if __name__ == '__main__':
+    model = get_unet((112, 112, 1), 2, weights=None)
